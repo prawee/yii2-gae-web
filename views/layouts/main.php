e105b8e9a278aaab73755f4634fe7297713fd053
Prawee Wongsa <prawee.w@integra8t.com>
2018-03-09 11:18:45 +0700

    using AppAsset to main layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,21 +3,30 @@
  * @Author: Prawee Wongsa <prawee.w@integra8t.com>
  * @Date: 14/2/2018 AD 15:25
  */
-?>
+use yii\helpers\Html;
+use i8\assets\AppAsset;
 
-<html>
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?=Yii::$app->language?>">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-        <title>REDEMPTION</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link href="https://storage.googleapis.com/oceanic-hold-176205.appspot.com/datepicker/bootstrap-datetimepicker.min.css" rel="stylesheet" />
+        <meta charset="<?=Yii::$app->charset?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?=Html::csrfMetaTags()?>
+        <title><?=Html::encode($this->title)?></title>
+        <?php $this->head()?>
     </head>
     <body>
+        <?php $this->beginBody()?>
         <div class="container">
             <div class="content">
                 <?=$content?>
             </div>
         </div>
+        <?php $this->endBody()?>
     </body>
 </html>
+<?php $this->endPage()?>

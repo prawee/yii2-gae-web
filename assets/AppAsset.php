51e70316c5e77f14939c5f2bca0f6a6b995f8654
Prawee Wongsa <prawee.w@integra8t.com>
2018-03-09 11:18:29 +0700

    create AppAsset

---
+++ assets/AppAsset.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author: Prawee Wongsa <prawee.w@integra8t.com>
+ * @Date: 8/3/2018 AD 17:16
+ */
+namespace i8\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+}
